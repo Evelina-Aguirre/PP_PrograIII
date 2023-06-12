21403ba1a78535e32e6f4447198c34f3a550948f
Eve <evelina.aguirre16@gmail.com>
2023-06-12 01:01:32 -0300

    Parte 2, pto 4 a y b

--- Hamburguesas/Busqueda.php
+++ Hamburguesas/Busqueda.php
@@ -10,7 +10,6 @@ class Busqueda
     }
 
 
-    //Informa si el array posee algún objeto con la característica buscada
     public static function BuscarObjetoEnArray($nombProp, $caracteristica, $array)
     {
         foreach ($array as $objebArray) {
@@ -20,17 +19,78 @@ class Busqueda
         }
         return false;
     }
-    //informa si existe o no una carac. en un objeto 
+
     public static function BuscaCaracteristica($nombProp, $caracteristica, $obj)
     {
         foreach ($obj as $clave => $valor) {
             if (strtolower($clave) == strtolower($nombProp) && strtolower($valor) == strtolower($caracteristica)) {
-                    return true;
+                return true;
             }
         }
 
         return false;
     }
 
+    public static function BuscarPorFecha($array, $fecha)
+    {
+        $cantidad = 0;
+        foreach ($array as $venta) {
 
+            $fechaHora = $venta['fecha'];
+            $parts = explode("_", $fechaHora);
+            $soloFecha = $parts[0];
+
+            if ($soloFecha == $fecha) {
+                $cantidad += $venta['cantidadPedido'];
+            }
+        }
+        return $cantidad;
+    }
+
+    public static function filtrarPorFecha($fechaInicio, $fechaFin)
+    {     
+        $fechaInicio = new DateTime($fechaInicio);
+        $fechaFin = new DateTime($fechaFin);
+
+        $fechas = array();
+
+        while ($fechaInicio <= $fechaFin) {
+            $fechas[] = $fechaInicio->format('d-m-Y');
+            $fechaInicio->modify('+1 day');
+        }
+        return $fechas;
+
+    }
+
+    /* public function ordenarPorNombre($venta1, $venta2)
+    {
+        return strcmp($venta1['nombreCliente'], $venta2['nombreCliente']);
+    }*/
+
+    public static function filtrarYOrdenarVentas($arrayVentas, $fechaInicio, $fechaFin)
+    {
+        $ventasEntreFechas = array();
+        $arrayFechas=self::filtrarPorFecha($fechaInicio, $fechaFin);
+    
+        foreach ($arrayVentas as $venta) {
+            $fechaHora = $venta['fecha'];
+            $parts = explode("_", $fechaHora);
+            $soloFecha = $parts[0];
+          
+            foreach($arrayFechas as $fecha)
+            {
+                if($soloFecha == $fecha)
+                {
+                    $ventasEntreFechas[]=$venta;
+                }
+            }
+        }
+       
+
+       usort($ventasEntreFechas, function ($venta1, $venta2) {
+            return strcmp($venta1['nombreCliente'], $venta2['nombreCliente']);
+        });
+
+        return $ventasEntreFechas;
+    }
 }
--- Hamburguesas/ConsultasVentas.php
+++ Hamburguesas/ConsultasVentas.php
@@ -3,23 +3,29 @@
 include_once 'Busqueda.php';
 include_once '_Json.php';
 include_once 'Venta.php';
+
 $fecha = isset($_POST['fecha']) ? $_POST['fecha'] : date('d-m-Y', strtotime('-1 day'));
+$fechaInicio = $_POST['fechaInicio'];
+$fechaFin = $_POST['fechaFin'];
 
 $arrayVentas = Archivos_Json::LeerJson(Venta::archivo);
 
-$cantidad = 0;
+$cantidad = Busqueda::BuscarPorFecha($arrayVentas,$fecha);
 
+$arrayOrdenado=Busqueda::filtrarYOrdenarVentas($arrayVentas, $fechaInicio, $fechaFin);
 
-foreach ($arrayVentas as $venta) {
+echo "Cantidad de hamburguesas vendidas el $fecha: $cantidad </br></br>";
 
-    $fechaHora = $venta['fecha'];
-    $parts = explode("_", $fechaHora);
-    $soloFecha = $parts[0];
+foreach($arrayOrdenado as $venta)
+{
     
-    if ($soloFecha == $fecha) {
-        echo "SE VENDIO : ", $venta['fecha'];
-        $cantidad += $venta['cantidadPedido'];
-    }
+    echo "\nID de venta: ", $venta['idVenta'],"</br>";
+    echo "Número de pedido:", $venta['numPedido'],"</br>";
+    echo "Fecha: ", $venta['fecha'],"</br>";
+    echo "Email: ", $venta['email'],"</br>";
+    echo "Nombre del cliente: ", $venta['nombreCliente'],"</br>";
+    echo "Tipo de pedido: ", $venta['tipoPedido'],"</br>";
+    echo "Aderezo del pedido: ", $venta['aderezoPedido'],"</br>";
+    echo "Cantidad de hamburguesas vendidas: ", $venta['cantidadPedido'],"</br>";
+    echo  "-----------------</br></br>";
 }
-
-echo "Cantidad de hamburguesas vendidas el $fecha: $cantidad";
