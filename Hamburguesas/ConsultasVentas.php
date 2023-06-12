5e0c43054add7194f15db735fd7fde2a9eafa67a
Eve <evelina.aguirre16@gmail.com>
2023-06-12 01:31:55 -0300

    Parte 2 - pto 4_ d y e

--- Hamburguesas/ConsultasVentas.php
+++ Hamburguesas/ConsultasVentas.php
@@ -8,6 +8,7 @@ $fecha = isset($_POST['fecha']) ? $_POST['fecha'] : date('d-m-Y', strtotime('-1 
 $fechaInicio = $_POST['fechaInicio'];
 $fechaFin = $_POST['fechaFin'];
 $usuario = $_POST['usuario'];
+$tipo = $_POST['tipo'];
 
 $arrayVentas = Archivos_Json::LeerJson(Venta::archivo);
 
@@ -22,3 +23,11 @@ Venta::ImprimirVenta($arrayOrdenado);
 echo "</BR></BR>COMPRAS DEL USUARIO --> ",$usuario,":</BR></BR>";
 $arrayPedidosDeUnUsuario=Busqueda::BuscarObjetoEnArrayYCargarLista('nombreCliente',$usuario,$arrayVentas);
 Venta::ImprimirVenta($arrayPedidosDeUnUsuario);
+
+echo "</BR></BR>COMPRAS DEL TIPO --> ",$tipo,":</BR></BR>";
+$arrayPorTipo=Busqueda::BuscarObjetoEnArrayYCargarLista('tipoPedido',$tipo,$arrayVentas);
+Venta::ImprimirVenta($arrayPorTipo);
+
+echo "</BR></BR>COMPRAS ADEREZO --> KETCHUP:</BR></BR>";
+$arrayPorAderezo=Busqueda::BuscarObjetoEnArrayYCargarLista('aderezoPedido','ketchup',$arrayVentas);
+Venta::ImprimirVenta($arrayPorAderezo);
